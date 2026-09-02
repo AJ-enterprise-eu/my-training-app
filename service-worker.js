eb6d7d1bccfe62fdfd4b2d2963e1f119ff3b6279
AJ-enterprise-eu <info@ajenterprise.eu>
2026-09-02 20:30:37 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="aj-training-smart-v5-5-1";
+const CACHE="aj-training-smart-v5-5-2";
 const ASSETS=["./","./index.html","./manifest.json","./app.js","./aj-logo.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
